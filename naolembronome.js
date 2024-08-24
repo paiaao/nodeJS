cd619ae0b01629425d4eb8f61551e099c8a876bb
paiaao <142060381+paiaao@users.noreply.github.com>
2024-08-23 22:03:40 -0300

    Add files via upload

--- naolembronome.js
+++ naolembronome.js
@@ -1,9 +1,20 @@
 const { sequelize, DataTypes} = require ('sequelise');
-const sequelize = new sequelize.define(
+const sequelize = new sequelize('sqlite::memory');
+
+const User = new sequelize.define(
     'User',
 {
+    //atributos da model
     firstname: {
         type: DataTypes.STRING,
         allowNull:false,
-    }
-})
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        //allowNull tem valor padrao true
+    },
+},
+{
+    //outras opcoes aqui
+},
+);
